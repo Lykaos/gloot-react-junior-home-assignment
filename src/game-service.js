ec546ce2cbfd9748bb7a97c455d4fd28b2c108f9
Eduardo Rodes <dragon11_r@hotmail.com>
2019-04-25 17:21:01 +0200

    Fix: Smaller identation

--- src/game-service.js
+++ src/game-service.js
@@ -6,48 +6,48 @@ const apiUrl = 'http://localhost:3000'
 
 export async function getGames() 
 {
-	const resp = await fetch(`${apiUrl}/game`)
-	return await resp.json()
+    const resp = await fetch(`${apiUrl}/game`)
+    return await resp.json()
 }
 
 export async function addGame(gameName, gameURL) 
 {
-	const resp = await fetch(`${apiUrl}/game`,
-	{
-		method: 'POST',
-		headers:
-		{
-			"Content-type": "application/json"
-		},
-		body: JSON.stringify(
-		{
-			"name": gameName,
-			"url": gameURL
-		})
-	})
-	return await resp.json()
+    const resp = await fetch(`${apiUrl}/game`,
+    {
+        method: 'POST',
+        headers:
+        {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(
+        {
+            "name": gameName,
+            "url": gameURL
+        })
+    })
+    return await resp.json()
 }
 
 export async function editGame(GameId, gameName, gameURL) 
 {
-	const resp = await fetch(`${apiUrl}/game/${GameId}`,
-	{
-		method: 'PUT',
-		headers:
-		{
-			"Content-type": "application/json"
-		},
-		body: JSON.stringify(
-		{
-			"name": gameName,
-			"url": gameURL
-		})
-	})
-	return await resp.json()
+    const resp = await fetch(`${apiUrl}/game/${GameId}`,
+    {
+        method: 'PUT',
+        headers:
+        {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(
+        {
+            "name": gameName,
+            "url": gameURL
+        })
+    })
+    return await resp.json()
 }
 
 export async function deleteGame(gameId) 
 {
-	const resp = await fetch(`${apiUrl}/game/${gameId}`, { method: 'DELETE' })
-	return await resp.json()
+    const resp = await fetch(`${apiUrl}/game/${gameId}`, { method: 'DELETE' })
+    return await resp.json()
 }
--- src/index.jsx
+++ src/index.jsx
@@ -10,84 +10,84 @@ import './style.css'
 
 class App extends React.Component 
 {
-	state = 
-	{
-		games: []
-	}
-
-	componentDidMount = async () => 
-	{
-		this.getGames()
-	}
-
-	getGames = async () => 
-	{
-		const games = await getGames()
-		this.setState({ games })
-	}
-
-	setNewNameState = () =>
-	{
-		this.setState({ newGameName: event.target.value })
-	}
-
-	setNewURLState = () =>
-	{
-		this.setState({ newGameURL: event.target.value })
-	}
-	  
-	addGame = async () => 
-	{
-		await addGame(this.state.newGameName, this.state.newGameURL)
-		this.getGames()
-	}
- 
-    editGame = async id => 
-	{
-		await editGame(id, this.state.newGameName, this.state.newGameURL)
-		this.getGames()
-	}
-
-	deleteGame = async id => 
-	{
-		await deleteGame(id)
-		this.getGames()
+    state = 
+    {
+        games: []
     }
 
-	render() 
-	{
-		return (
-			<div>
-				<div id='header'>
-					<div id='title'>Game Library
-					<button id='addGameButton' onClick={() => this.addGame()}>+</button>
-					<input className='inputField' placeholder="Image URL" onChange={this.setNewURLState}/>
-					<input className='inputField' placeholder="Game name" onChange={this.setNewNameState}/>
-					</div>
-				</div>
+    componentDidMount = async () => 
+    {
+        this.getGames()
+    }
 
-				<div id='content'>
-					{this.state.games.map(game => 
-					(
-						<div>
-							<button className='deleteButton' onClick={() => this.deleteGame(game.id)}>Delete</button>
-							<button className='editButton' onClick={() => this.editGame(game.id)}>Edit</button>
-							<div className='itemList' key={game.id} style=
-								{{
-									backgroundImage: `url(${game.url})`, 
-									backgroundSize: '100%', 
-									backgroundPosition: 'center center',
-									backgroundRepeat: 'no-repeat',
-									backgroundSize: 'cover'
-								}}>
-								<label className='gameLabel'>{game.name}</label>		
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
-		)
-	}
+    getGames = async () => 
+    {
+        const games = await getGames()
+        this.setState({ games })
+    }
+
+    setNewNameState = () =>
+    {
+        this.setState({ newGameName: event.target.value })
+    }
+
+    setNewURLState = () =>
+    {
+        this.setState({ newGameURL: event.target.value })
+    }
+      
+    addGame = async () => 
+    {
+        await addGame(this.state.newGameName, this.state.newGameURL)
+        this.getGames()
+    }
+ 
+    editGame = async id => 
+    {
+        await editGame(id, this.state.newGameName, this.state.newGameURL)
+        this.getGames()
+    }
+
+    deleteGame = async id => 
+    {
+        await deleteGame(id)
+        this.getGames()
+    }
+
+    render() 
+    {
+        return (
+            <div>
+                <div id='header'>
+                    <div id='title'>Game Library
+                    <button id='addGameButton' onClick={() => this.addGame()}>+</button>
+                    <input className='inputField' placeholder="Image URL" onChange={this.setNewURLState}/>
+                    <input className='inputField' placeholder="Game name" onChange={this.setNewNameState}/>
+                    </div>
+                </div>
+
+                <div id='content'>
+                    {this.state.games.map(game => 
+                    (
+                        <div>
+                            <button className='deleteButton' onClick={() => this.deleteGame(game.id)}>Delete</button>
+                            <button className='editButton' onClick={() => this.editGame(game.id)}>Edit</button>
+                            <div className='itemList' key={game.id} style=
+                                {{
+                                    backgroundImage: `url(${game.url})`, 
+                                    backgroundSize: '100%', 
+                                    backgroundPosition: 'center center',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundSize: 'cover'
+                                }}>
+                                <label className='gameLabel'>{game.name}</label>		
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('app-container'))
